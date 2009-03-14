c523d871e3058fed77010f029343b27ee5c9af72
Michael Stahnke <mastahnke@gmail.com>
2009-03-14 18:50:57 -0500

    Minor cleanup

--- cronjobs/manifests/init.pp
+++ cronjobs/manifests/init.pp
@@ -1,10 +1,12 @@
 # Install cronjobs
 
-
 # Should probably ensure this is a redhat/fedora/centos based system
-file { "/etc/cron.daily/00yum":
+
+  file { "cron-yum":
+    path => '/etc/cron.daily/00yum', 
     owner => root, 
     group => root, 
     mode => 755,
+    require => Package['crontabs'], 
     source => "puppet:///cronjobs/00yum"
-}
+  }
