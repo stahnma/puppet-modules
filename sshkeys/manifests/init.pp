5385ae28e15e1ea4169e113309b16bdd2b287f3e
Michael Stahnke <mastahnke@gmail.com>
2009-03-14 15:17:11 -0500

    Testing module

--- sshkeys/manifests/init.pp
+++ sshkeys/manifests/init.pp
@@ -1,16 +1,17 @@
 # Simple module to deploy public ssh key
 #
-file { "/root/.ssh":
+class sshkeys::root { 
+  file { "/root/.ssh":
        ensure => directory,
        owner => root,
        group => root,
        mode => 700
-}
+  }
 
-file { "/root/.ssh/authorized_keys":
+  file { "/root/.ssh/authorized_keys":
        owner => root,
        group => root,
        mode => 400,
        source => "puppet://$server/files/root/authorized_keys"
+  }
 }
-
