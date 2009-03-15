0d4d7d3b733d488416cdeb730d30e40d822db38f
Michael Stahnke <mastahnke@gmail.com>
2009-03-14 21:14:07 -0500

    Finished first pass at puppet client module

--- puppetclient/manifests/init.pp
+++ puppetclient/manifests/init.pp
@@ -4,17 +4,23 @@
 # needs to ensure that puppetd is running
 # should check to ensure that puppet is the latest?
 
+  $pserver = "hart.stahnkage.com"
 # should probably use a template to specify the server???
-  file { "/etc/sysconfig/puppet":
-       source => "puppet:///puppetclient/puppet" ,
-       mode => 444,
-       owner => root,
-       group => root,
-  }
-
   service { "puppet":
       ensure => true,
       enable => true,
       hasstatus => true,
       hasrestart => true,
+  }
+
+  # definition that sets up /etc/sysconfig/puppet
+  file { "/etc/sysconfig/puppet": 
+    owner => root,
+    group => root,
+    content => template('puppetclient/sysconfig-puppet.erb')
+  }
+
+  exec { "/etc/init.d/puppet stop; /etc/init.d/puppet start":
+    subscribe => File["/etc/sysconfig/puppet"],
+    refreshonly => true
   }
