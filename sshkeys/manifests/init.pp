196bf67019a70cdecd7140af249d08a133f12ef4
Michael Stahnke <mastahnke@gmail.com>
2009-10-05 22:10:34 -0500

    Changed from strict module to a class.  Now sshkeys::root.

--- sshkeys/manifests/init.pp
+++ sshkeys/manifests/init.pp
@@ -1,15 +1,4 @@
-# Simple module to deploy public ssh key
-# to root user
-  file { "/root/.ssh":
-       ensure => directory,
-       owner => root,
-       group => root,
-       mode => 700
-  }
-
-  file { "/root/.ssh/authorized_keys":
-       owner => root,
-       group => root,
-       mode => 400,
-       source => "puppet:///sshkeys/root/authorized_keys"
-  }
+# Class: sshkeys
+#
+# This is a place-holder class for subclasses per user or function for SSH key distribution. 
+class sshkeys {  } 
---
+++ sshkeys/manifests/root.pp
@@ -0,0 +1,31 @@
+# Class: sshkeys::root
+# 
+# Simple class/module to deploy public ssh keys for root user
+# 
+# Actions: 
+#  Installs contents for root/authorized_keys into root/.ssh/authorized_keys
+#
+# Files:
+#  An authorized_key file is distributed 
+#
+# Requires:
+#  There is an implicit requires on an OpenSSH server, but it is not specified in this class.
+#
+# Sample usage:
+#  include 'sshkeys::root'
+#
+class sshkeys::root { 
+  file { "/root/.ssh":
+       ensure => directory,
+       owner => root,
+       group => root,
+       mode => 700
+  }
+
+  file { "/root/.ssh/authorized_keys":
+       owner => root,
+       group => root,
+       mode => 400,
+       source => "puppet:///sshkeys/root/authorized_keys"
+  }
+}
