5bb41d49907f39ba3a2d0f041363779314ce4b4e
stahnma <stahnma@websages.com>
2010-02-12 21:09:22 -0600

    Fixing dependencies.

--- packages/manifests/rhel/epel.pp
+++ packages/manifests/rhel/epel.pp
@@ -1,10 +1,12 @@
-class packages::rhel::epel { 
-  exec { "setup-epel":  
+class packages::rhel::epel {
+  exec { "setup-epel":
     unless => "/bin/rpm -q epel-release",
-    command => "/bin/rpm -U http://mirrors.xmission.com/fedora/epel/5Server/i386/epel-release-5-3.noarch.rpm"
+    command => "/bin/rpm -U http://mirrors.xmission.com/fedora/epel/5Server/i386/epel-release-5-3.noarch.rpm",
+    notify => Package[epel-release],
   }
 
   package { "epel-release":
      ensure => latest,
+     require => Exec["setup-epel"]
   }
 }
