75b504fdca984e8f2c0a5aaea34d4df1157d5060
stahnma <stahnma@websages.com>
2010-02-14 10:00:26 -0600

    Adding screen as a server package.

--- packages/manifests/rhel/server.pp
+++ packages/manifests/rhel/server.pp
@@ -1,7 +1,7 @@
 # Package installation and control
 class packages::rhel::server { 
   include packages::rhel::server
-  $serverPkgs = [ 'curl', 'httpd', 'irssi',  'logrotate', 'net-snmp-utils'  ] 
+  $serverPkgs = [ 'curl', 'httpd', 'irssi',  'logrotate', 'net-snmp-utils', 'screen'  ] 
   Package {
     require => Class["packages::rhel::epel"],
   }
