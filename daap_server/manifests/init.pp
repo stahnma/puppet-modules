98c2865939c330651447ffbf6ec6a01b4a728f78
stahnma <stahnma@websages.com>
2010-02-15 23:24:55 -0500

    Comments for iptables issues.

--- daap_server/manifests/init.pp
+++ daap_server/manifests/init.pp
@@ -1,3 +1,7 @@
+#IPTABLES RULES
+#   /sbin/iptables -I INPUT -p tcp --dport 3689 -j ACCEPT
+#   /sbin/iptables -I INPUT -p udp --dport 3689 -j ACCEPT 
+
 class daap_server {
   include "daap_server::setup"
 }  
