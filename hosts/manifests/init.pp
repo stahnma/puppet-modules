f3b71c9b3fca7ca3669349f616c00e93df62bad2
stahnma <stahnma@duo.stahnkage.com>
2010-05-05 22:54:08 -0400

    New module for managing /etc/hosts

---
+++ hosts/manifests/init.pp
@@ -0,0 +1,51 @@
+# This class just sets up /etc/hosts
+# Class: applications:lssm
+# Author: Michael Stahnke 
+# Copyright: (c) 2010
+# License: BSD
+#
+# Parameters:
+#   None
+#
+# Actions:
+#  Sets up /etc/hosts
+#    localhost
+#    puppet server
+#    fqdn
+#
+# Requires:
+#  - Nothing
+#
+# Sample Usage:
+#
+#  include 'hosts'
+# 
+class hosts { 
+  file { "/etc/hosts":
+    ensure => present,
+  }
+  host { 'localhost.localdomain':
+    ip => '127.0.0.1',
+    host_aliases => ['localhost'],
+    ensure => 'present',
+    target => '/etc/hosts'
+  }
+  host { 'localhost6.localdomain6':
+    ip => '::1',
+    host_aliases => ['localhost6'],
+    ensure => 'present',
+    target => '/etc/hosts'
+  }
+  host { $fqdn:
+    ip => $ipaddress,
+    host_aliases => [$hostname],
+    ensure => 'present',
+    target => '/etc/hosts'
+  }
+  host { 'puppet.stahnkage.com':
+    ip => '192.168.1.1.109',
+    host_aliases => ['puppet', 'www', 'ram' ],
+    ensure => 'present',
+    target => '/etc/hosts'
+  }
+} 
