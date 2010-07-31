8eabbdd04e4f0987cfc72cf4707f11f9f315a5a7
stahnma <stahnma@websages.com>
2010-07-30 22:02:35 -0500

    Added Subversion instead of svn as the package was misnamed.

--- rpmdev/manifests/init.pp
+++ rpmdev/manifests/init.pp
@@ -2,7 +2,7 @@
 # Require mock module 
 class rpmdev { 
   # Fedora RPM Devel Packages
-  $fpkgs = [ "rpmdevtools", "fedora-packager", "git", "svn" ] 
+  $fpkgs = [ "rpmdevtools", "fedora-packager", "git", "subversion" ] 
 
   package { $fpkgs: 
     ensure => installed,
