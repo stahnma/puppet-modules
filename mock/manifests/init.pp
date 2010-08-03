931b9499353b2bf4099cfd95ed20a46a20451606
stahnma <stahnma@websages.com>
2010-08-03 07:13:23 -0500

    Fixed up mock module to manage my default user a little more.

--- mock/manifests/init.pp
+++ mock/manifests/init.pp
@@ -4,13 +4,24 @@ class mock {
     ensure => present,
   }
 
+  @file { "/home/stahnma":
+    owner => 'stahnma',
+    group => 'stahnma',
+    mode => 755,
+    ensure => directory,
+    require => Group['stahnma'],
+    
+  }
+
   @user { "stahnma": 
     ensure => present,
+    home => '/home/stahnma',
     comment => "Michael Stahnke",
     uid => 13802,
     shell => "/bin/bash",
     gid => "13802",
     groups =>  [ "stahnma", "mock", "wheel" ],
+    require => File['/home/stahnma'],
   }
 
   @group { "mock": 
@@ -22,5 +33,5 @@ class mock {
     gid => 13802,
   }
   
-  realize(User[stahnma], Group[mock], Group[stahnma])
+  realize(User[stahnma], Group[mock], Group[stahnma], File['/home/stahnma'])
 }
