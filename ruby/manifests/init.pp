17980249aaeed5272e7615b1cb00a5f1c02a4d50
stahnma <stahnma@websages.com>
2010-09-09 16:46:37 -0400

    Adding ruby-ri as a package in ruby module.

--- ruby/manifests/init.pp
+++ ruby/manifests/init.pp
@@ -1,7 +1,7 @@
 
 
 class ruby { 
-  $pkgs = [ 'ruby', 'rubygems', 'rubygem-rake', 'ruby-devel', 'ruby-ldap' ] 
+  $pkgs = [ 'ruby', 'rubygems', 'rubygem-rake', 'ruby-devel', 'ruby-ldap', 'rubygem-shotgun', 'rubygem-sinatra', 'ruby-ri' ] 
 
   package { $pkgs : 
     ensure => installed,
