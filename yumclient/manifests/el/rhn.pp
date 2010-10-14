19d7ce4df17499fa3d36415f01b73b26a062fd5c
Michael Stahnke <stahnma@sbox01.fpd.cat.com>
2010-10-14 10:07:21 -0500

    Getting RHEL setup with gpg keys.

--- yumclient/manifests/el/rhn.pp
+++ yumclient/manifests/el/rhn.pp
@@ -1,5 +1,62 @@
 class yumclient::el::rhn { 
   notice("RHN should be configured and setup.")
   # Also configure the default PKI 
+  File {
+     owner => 'root',
+     group => 'root',
+     mode => '644',
+  }
+
+  file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release":
+    ensure => present,
+    source => "puppet:///modules/yumclient/RPM-GPG-KEY-redhat-release-${operatingsystemversion}", 
+  }
+
+ file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta":
+    ensure => present,
+    source => "puppet:///modules/yumclient/RPM-GPG-KEY-redhat-beta-${operatingsystemversion}", 
+  }
+
+  file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-rhx":
+    ensure => present,
+    source => "puppet:///modules/yumclient/RPM-GPG-KEY-redhat-rhx", 
+  }
+
+  file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-auxiliary":
+    ensure => present,
+    source => "puppet:///modules/yumclient/RPM-GPG-KEY-redhat-auxiliary", 
+  }
+ 
+ file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-former":
+    ensure => present,
+    source => "puppet:///modules/yumclient/RPM-GPG-KEY-redhat-former", 
+  }
+  
+  yumclient::rpm_gpg_key{ "redhat-release-$operatingsystemversion":
+     path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release",
+  }
+
+  yumclient::rpm_gpg_key{ "redhat-beta-$operatingsystemversion":
+     path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta",
+  }
+
+  yumclient::rpm_gpg_key{ "redhat-rhx":
+     path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-rhx",
+  }
+
+  yumclient::rpm_gpg_key{ "redhat-auxiliary":
+     path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-auxiliary",
+  }
+
+  # Leaving this out as it should not be needed
+  #yumclient::rpm_gpg_key{ "redhat-former":
+  #   path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-former",
+  #}
+
+  
+  
+ 
+
+
 
 }
