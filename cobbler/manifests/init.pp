7850a3c5a21072d38c7cefe00c0e6da2a9d8f0a1
stahnma <stahnma@websages.com>
2011-02-13 22:46:26 -0600

    More work on cobbler module.  Now using an iptables module from forge.puppetlabs.com also.  Seems to be working well. Cobbler module still not complete.

--- cobbler/manifests/init.pp
+++ cobbler/manifests/init.pp
@@ -16,5 +16,74 @@ class cobbler {
     enable => true,
     require => Service['cobblerd'],
   }
+
+  file { "/etc/cobbler/settings":
+    ensure => present,
+    mode => 0644,
+    owner => root,
+    group => root,
+    content => template('cobbler/settings.erb'), 
+    notify => Service['cobblerd'],
+  }
+
+  service { "tftp": 
+    enable => true, 
+    require => Package[$cobbler_packages],
+  }
+
+  service { "rsync":
+    enable => true,
+    require => Package[$cobbler_packages],
+  }
+
+#  package { "debmirror":
+#    ensure => installed,
+#  }
+
+  exec { "getloaders":
+    command => 'cobbler get-loaders',
+    path => [ "/usr/bin", "/usr/sbin", "/bin", "/sbin" ] ,
+    unless => " test -f /var/lib/cobbler/loaders/yaboot ",
+    require => Package[$cobbler_packages],
+  }
+     
+   
+  iptables { "open httpd":
+    dport => "80",
+    source => "0.0.0.0",
+    destination => "0.0.0.0",
+    jump => "ACCEPT",
+    require => Service['httpd'],
+  }
+
+  iptables { "open tftp":
+    dport => "69",
+    source => "0.0.0.0",
+    destination => "0.0.0.0",
+    jump => "ACCEPT",
+    require => Service['tftp'],
+  }
+
+  iptables { "open ssl":
+    dport => "443",
+    source => "0.0.0.0",
+    destination => "0.0.0.0",
+    jump => "ACCEPT",
+    require => Service['httpd'],
+  }
+
+  iptables { "open 25151":
+    dport => "25151",
+    source => "0.0.0.0",
+    destination => "0.0.0.0",
+    jump => "ACCEPT",
+    require => Package[$cobbler_packages],
+  }
+
+
+   
+    
+
+
 } 
 
